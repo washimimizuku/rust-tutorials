268bd9fae17971af0a664b77ac4fa649dd87aed5
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-15 16:28:07 +0200

    Add match operator to chapter 6

---
+++ chapter6/match_operator/src/main.rs
@@ -0,0 +1,119 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Arizona,
+    Arkansas,
+    California,
+    Colorado,
+    Connecticut,
+    Delaware,
+    Florida,
+    Georgia,
+    Hawaii,
+    Idaho,
+    Illinois,
+    Indiana,
+    Iowa,
+    Kansas,
+    Kentucky,
+    Louisiana,
+    Maine,
+    Maryland,
+    Massachusetts,
+    Michigan,
+    Minnesota,
+    Mississippi,
+    Missouri,
+    Montana,
+    Nebraska,
+    Nevada,
+    NewHampshire,
+    NewJersey,
+    NewMexico,
+    NewYork,
+    NorthCarolina,
+    NorthDakota,
+    Ohio,
+    Oklahoma,
+    Oregon,
+    Pennsylvania,
+    RhodeIsland,
+    SouthCarolina,
+    SouthDakota,
+    Tennessee,
+    Texas,
+    Utah,
+    Vermont,
+    Virginia,
+    Washington,
+    WestVirginia,
+    Wisconsin,
+    Wyoming,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+// Matching an enum
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+// Matching with Option<T>
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    // Matching an enum
+    let penny = Coin::Penny;
+    println!("The value of a penny is {} cents.", value_in_cents(penny));
+    let nickel = Coin::Nickel;
+    println!("The value of a nickel is {} cents.", value_in_cents(nickel));
+    let dime = Coin::Dime;
+    println!("The value of a dime is {} cents.", value_in_cents(dime));
+    let quarter = Coin::Quarter(UsState::Alaska);
+    println!("The value of a quarter is {} cents.", value_in_cents(quarter));
+
+    // Matching with Option<T>
+    let five = Some(5);
+    let six = plus_one(five);
+    println!("The value of six is {:?}.", six);
+    let none = plus_one(None);
+    println!("The value of none is {:?}.", none);
+
+    // The _ Placeholder
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        2 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => println!("number not found"), // _ matches any other
+    }
+
+    let state = UsState::Alaska;
+    match state {
+        UsState::Alabama => println!("State is Alabama."),
+        _ => println!("State not Alabama!"), // _ matches any other
+    }
+}
