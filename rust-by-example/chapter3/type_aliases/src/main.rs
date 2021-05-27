446a6a84485690a5b9f361c77ea9c5e5838c600a
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-27 21:13:59 +0200

    Add type aliases to chapter 3

---
+++ rust-by-example/chapter3/type_aliases/src/main.rs
@@ -0,0 +1,26 @@
+enum VeryVerboseEnumOfThingsToDoWithNumbers {
+    Add,
+    Subtract,
+}
+
+impl VeryVerboseEnumOfThingsToDoWithNumbers {
+    fn run(&self, x: i32, y: i32) -> i32 {
+        match self {
+            Self::Add => x + y,
+            Self::Subtract => x - y,
+        }
+    }
+}
+
+// Creates a type alias
+type Operations = VeryVerboseEnumOfThingsToDoWithNumbers;
+
+fn main() {
+    // We can refer to each variant via its alias, not its long and inconvenient
+    // name.
+    let add = Operations::Add;
+    println!("Add: {}", add.run(4, 2));
+
+    let subtract = Operations::Subtract;
+    println!("Subtract: {}", subtract.run(4, 2));
+}
