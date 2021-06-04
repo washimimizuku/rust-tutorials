7da40929ecd3e78306a5e2af0b3896f19bdef6a0
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-06-04 19:28:29 +0100

    Add for loops to chapter 8

---
+++ rust-by-example/chapter8-flow-of-control/for_loops/src/main.rs
@@ -0,0 +1,73 @@
+fn main() {
+    // `n` will take the values: 1, 2, ..., 100 in each iteration
+    for n in 1..101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+
+    // `n` will take the values: 1, 2, ..., 100 in each iteration
+    for n in 1..=100 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+
+    // For and iterators
+
+    // iter - This borrows each element of the collection through
+    // each iteration. Thus leaving the collection untouched and
+    // available for reuse after the loop.
+    let names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.iter() {
+        match name {
+            &"Ferris" => println!("There is a rustacean among us!"),
+            // TODO ^ Try deleting the & and matching just "Ferris"
+            _ => println!("Hello {}", name),
+        }
+    }
+    
+    println!("names: {:?}", names);
+
+    // into_iter - This consumes the collection so that on each
+    // iteration the exact data is provided. Once the collection
+    // has been consumed it is no longer available for reuse as it
+    // has been 'moved' within the loop.
+    let names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.into_iter() {
+        match name {
+            "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+    
+    // println!("names: {:?}", names);
+    // FIXME ^ Comment out this line
+
+    // iter_mut - This mutably borrows each element of the collection,
+    // allowing for the collection to be modified in place.
+    let mut names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.iter_mut() {
+        *name = match name {
+            &mut "Ferris" => "There is a rustacean among us!",
+            _ => "Hello",
+        }
+    }
+
+    println!("names: {:?}", names);
+}
