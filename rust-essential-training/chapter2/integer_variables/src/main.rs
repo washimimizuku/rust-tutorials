109d0eafa0f118ac985014e720152a0dc639f8fc
Nuno Barreto <nbarreto@gmail.com>
2022-01-31 19:59:10 +0100

    Add integer variables examples

---
+++ rust-essential-training/chapter2/integer_variables/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // i32 by default (Signed inteer 32 bits)
+    let x = -10;
+    println!("x is {}", x);
+    
+    // Unsigned integer 8 bits
+    let a: u8 = 10;
+    println!("x is {}", a);
+}
