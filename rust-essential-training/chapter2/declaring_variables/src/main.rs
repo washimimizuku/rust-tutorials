c10ac0215bf12297c6bf47fd97a8c24d7e1d743c
Nuno Barreto <nbarreto@gmail.com>
2022-01-31 19:46:04 +0100

    Add muttable variables

--- rust-essential-training/chapter2/declaring_variables/src/main.rs
+++ rust-essential-training/chapter2/declaring_variables/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
+    // Imuttable variable
     let x = 10;
     println!("x is {}", x);
+    
+    // Muttable variable
+    let mut y = 20;
+    println!("x is {}", y);
+    y = 30;
+    println!("x is {}", y);
 }
