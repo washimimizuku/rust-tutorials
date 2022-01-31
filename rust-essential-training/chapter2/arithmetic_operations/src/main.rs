cf609e7aa9d57cd7a0687847c39f889132c096cb
Nuno Barreto <nbarreto@gmail.com>
2022-01-31 20:48:08 +0100

    Add arithmetic operations example

---
+++ rust-essential-training/chapter2/arithmetic_operations/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    let a = 10;
+    let b = 3;
+
+    // Adition
+    let c = a + b;
+    println!("c is {}", c);
+
+    // Subtraction
+    let d = a - b;
+    println!("d is {}", d);
+
+    // Multiplication
+    let e = a * b;
+    println!("e is {}", e);
+
+    // Division of integers returns an integer
+    let f = a / b;
+    println!("f is {}", f);
+
+    // Remainer
+    let g = a % b;
+    println!("g is {}", g);
+
+    // Division of floats
+    let x = 10.0;
+    let y = 3.0;
+
+    let z = x / y;
+    println!("z is {}", z);
+}
