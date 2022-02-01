7d9f95e50d75fb250f084b2a076ad327d78e510b
Nuno Barreto <nbarreto@gmail.com>
2022-02-01 21:21:43 +0100

    Add boolean variables example

---
+++ rust-essential-training/chapter2/boolean_variables/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let a = true;
+    let b = false;
+
+    println!("a is {} and b is {}", a, b);
+    println!("NOT a is {}", !a);
+    println!("a AND B is {}", a & b);
+    println!("a OR B is {}", a | b);
+    println!("a XOR B is {}", a ^ b);
+}
