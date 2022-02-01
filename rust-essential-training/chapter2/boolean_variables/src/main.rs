945f172116a29b41cc7c686d3839f07e51bb88a8
Nuno Barreto <nbarreto@gmail.com>
2022-02-01 21:26:24 +0100

    Add short-circuiting bitwise operators

--- rust-essential-training/chapter2/boolean_variables/src/main.rs
+++ rust-essential-training/chapter2/boolean_variables/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     let a = true;
     let b = false;
 
+    // Bitwise operations on booleans
     println!("a is {} and b is {}", a, b);
     println!("NOT a is {}", !a);
     println!("a AND B is {}", a & b);
@@ -9,6 +10,16 @@ fn main() {
     println!("a XOR B is {}", a ^ b);
 
     let c = (a ^ b) | (a & b);
-    
+
     println!("c is {}", c);
+
+    // Short-circuiting OR (only executes right side if necessary)
+    let d = (a ^ b) || (a & b);
+
+    println!("d is {}", d);
+
+    // Short-circuiting AND (only executes right side if necessary)
+    let e = (a & b) && (a ^ b);
+
+    println!("e is {}", e);
 }
