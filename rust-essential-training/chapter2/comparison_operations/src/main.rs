b042d675b030c5798e6b47a07b2861e8173cdeb4
Nuno Barreto <nbarreto@gmail.com>
2022-02-01 21:32:13 +0100

    Add booleans example

--- rust-essential-training/chapter2/comparison_operations/src/main.rs
+++ rust-essential-training/chapter2/comparison_operations/src/main.rs
@@ -1,6 +1,19 @@
 fn main() {
+    // With integers
     let a = 1;
     let b = 2;
+
+    println!("a is {} and b is {}", a, b);
+    println!("a EQUAL TO b is {}", a == b);
+    println!("a NOT EQUAL TO b is {}", a != b);
+    println!("a GREATER THAN b is {}", a > b);
+    println!("a GREATER THAN OR EQUAL TO b is {}", a >= b);
+    println!("a LESS THAN b is {}", a < b);
+    println!("a LESS THAN OR EQUAL TO b is {}", a <= b);
+
+    // With booleans
+    let a = true;
+    let b = false;
 
     println!("a is {} and b is {}", a, b);
     println!("a EQUAL TO b is {}", a == b);
