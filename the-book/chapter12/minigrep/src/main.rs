5266e3563acc71e607b4cc0a3aad0835953d3c4e
Nuno Barreto <nbarreto@visionapartments.com>
2022-03-28 12:07:09 +0200

    Store args parameters

--- the-book/chapter12/minigrep/src/main.rs
+++ the-book/chapter12/minigrep/src/main.rs
@@ -2,5 +2,12 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    
+    println!("Args: {:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+    
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 }
