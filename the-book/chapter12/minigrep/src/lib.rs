280098e1abc5f2d4bb98dc755b017ea7d42430b1
Nuno Barreto <nbarreto@visionapartments.com>
2022-03-28 13:41:57 +0200

    Use search case insensitive function

--- the-book/chapter12/minigrep/src/lib.rs
+++ the-book/chapter12/minigrep/src/lib.rs
@@ -1,9 +1,11 @@
 use std::error::Error;
 use std::fs;
+use std::env;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -14,17 +16,29 @@ impl Config {
 
         let query = args[1].clone();
         let filename = args[2].clone();
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
     
-        Ok(Config { query, filename })
+        Ok(Config {
+            query, 
+            filename,
+            case_sensitive,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
     println!("With text:\n{}\n", contents);
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
     
     println!("Found in lines:");
-    for line in search(&config.query, &contents) {
+    for line in results {
         println!("{}", line);
     }
 
