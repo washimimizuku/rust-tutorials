2bd6f71fd9ea2fb7d590477428a1c4f14dc17fdc
Nuno Barreto <nbarreto@visionapartments.com>
2022-03-31 14:08:04 +0200

    Use Box for recursive types

--- the-book/chapter15/boxt/src/main.rs
+++ the-book/chapter15/boxt/src/main.rs
@@ -1,4 +1,15 @@
+use crate::List::{Cons, Nil};
+
 fn main() {
+    // Simple use of Box<T>
     let b = Box::new(5);
     println!("b = {}", b);
+
+    // Use of Box<T> for recursive types
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+}
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
 }
