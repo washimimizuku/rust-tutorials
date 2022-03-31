109c507f832835d3f755b870f51738eb7c683a21
Nuno Barreto <nbarreto@visionapartments.com>
2022-03-31 14:28:19 +0200

    Dereference with custom smart pointer

--- the-book/chapter15/dereference/src/main.rs
+++ the-book/chapter15/dereference/src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 fn main() {
     // Using dereference
     let x = 5;
@@ -5,4 +7,35 @@ fn main() {
 
     assert_eq!(5, x);
     assert_eq!(5, *y);
+
+    // Using Box<T>
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    // Using MyBox<T>
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+// Defining our own smart pointer
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
