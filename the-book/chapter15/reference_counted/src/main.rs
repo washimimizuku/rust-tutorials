cc83ad346221a7c84447110086b18025044c6271
Nuno Barreto <nbarreto@visionapartments.com>
2022-04-01 10:37:52 +0200

    Use Rc to share data

--- the-book/chapter15/reference_counted/src/main.rs
+++ the-book/chapter15/reference_counted/src/main.rs
@@ -1,3 +1,13 @@
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
 fn main() {
-    println!("Hello, world!");
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
 }
