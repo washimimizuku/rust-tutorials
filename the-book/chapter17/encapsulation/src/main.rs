ced5271bb77c6024d4781937ec28c5147daba34b
Nuno Barreto <nbarreto@visionapartments.com>
2022-04-13 16:30:44 +0200

    Create usage example

--- the-book/chapter17/encapsulation/src/main.rs
+++ the-book/chapter17/encapsulation/src/main.rs
@@ -1,3 +1,23 @@
+use encapsulation::AverageCollection;
+
 fn main() {
-    println!("Hello, world!");
+    let mut average_collection = AverageCollection::new();
+    println!("{:?}", average_collection);
+    println!("Average: {}", average_collection.average());
+
+    average_collection.add(1);
+    average_collection.add(2);
+    average_collection.add(3);
+    println!("{:?}", average_collection);
+    println!("Average: {}", average_collection.average());
+
+    average_collection.remove();
+    println!("{:?}", average_collection);
+    println!("Average: {}", average_collection.average());
+
+    average_collection.remove();
+    average_collection.remove();
+    println!("{:?}", average_collection);
+    println!("Average: {}", average_collection.average());
+
 }
