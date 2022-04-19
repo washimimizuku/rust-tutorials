bf1c7fa2665b4717e7029b808e95672f0cb9ec19
Nuno Barreto <nbarreto@visionapartments.com>
2022-04-19 14:25:49 +0200

    Add example for Dereferencing a Raw Pointer

--- the-book/chapter19/unsafe_rust/src/main.rs
+++ the-book/chapter19/unsafe_rust/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    // Creating raw pointers from references
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+
+    // Creating a raw pointer to an arbitrary memory address
+    let address = 0x012345usize;
+    let r = address as *const i32;
+
+    // Would work but give a Segmentation fault
+    /*
+    unsafe {
+        println!("r is: {}", *r);
+    }
+    */
 }
