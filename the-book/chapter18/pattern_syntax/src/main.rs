a823d154cbfaaab076793cf3776856883bb6a979
Nuno Barreto <nbarreto@visionapartments.com>
2022-04-19 13:42:07 +0200

    Add examples of extra conditionals with match guards

--- the-book/chapter18/pattern_syntax/src/main.rs
+++ the-book/chapter18/pattern_syntax/src/main.rs
@@ -192,7 +192,33 @@ fn main() {
         }
     }
 
+    // Extra Conditionals with Match Guards
+    let num = Some(4);
 
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {} is even", x),
+        Some(x) => println!("The number {} is odd", x),
+        None => (),
+    }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {}", n),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {}", x, y);
+
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
+    }
 }
 
 fn foo(_: i32, y: i32) {
