bd53f9a73939e1ad81ff242f15a9f01bec97d9f8
Nuno Barreto <nbarreto@visionapartments.com>
2022-04-19 14:28:38 +0200

    Add example for Calling an Unsafe Function or Method

--- the-book/chapter19/unsafe_rust/src/main.rs
+++ the-book/chapter19/unsafe_rust/src/main.rs
@@ -1,3 +1,19 @@
+use std::slice;
+
+fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
+
 fn main() {
     // Creating raw pointers from references
     let mut num = 5;
@@ -20,4 +36,26 @@ fn main() {
         println!("r is: {}", *r);
     }
     */
+
+    // Calling an Unsafe Function or Method
+    unsafe fn dangerous() {}
+
+    unsafe {
+        dangerous();
+    }
+
+    // Creating a Safe Abstraction over Unsafe Code
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let r = &mut v[..];
+
+    let (a, b) = r.split_at_mut(3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+
+    let address = 0x01234usize;
+    let r = address as *mut i32;
+
+    let values: &[i32] = unsafe { slice::from_raw_parts_mut(r, 10000) };
 }
