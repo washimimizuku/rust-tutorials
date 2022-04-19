d0931e2d001e69c09d9225ee05223838d6037787
Nuno Barreto <nbarreto@visionapartments.com>
2022-04-19 15:55:39 +0200

    Validate HTTP request

--- the-book/chapter20/hello/src/main.rs
+++ the-book/chapter20/hello/src/main.rs
@@ -16,25 +16,41 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
 
     println!("####### REQUEST BEGIN #######");
     println!("{}", String::from_utf8_lossy(&buffer[..]));
     println!("####### REQUEST END #######");
 
-    let contents = fs::read_to_string("./hello.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
 
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("./hello.html").unwrap();
+    
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+    
+        println!("####### RESPONSE BEGIN #######");
+        println!("{}", response);
+        println!("####### RESPONSE END #######");
+    
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
 
-    println!("####### RESPONSE BEGIN #######");
-    println!("{}", response);
-    println!("####### RESPONSE END #######");
+        let response = format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            status_line,
+            contents.len(),
+            contents
+        );
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
