d992c465b7a6996bcc1198433e9870b01329d2dd
Nuno Barreto <nbarreto@visionapartments.com>
2022-04-19 13:39:38 +0200

    Add examples of ignoring values in a pattern

--- the-book/chapter18/pattern_syntax/src/main.rs
+++ the-book/chapter18/pattern_syntax/src/main.rs
@@ -132,4 +132,69 @@ fn main() {
         "Position: x = {}, y = {}",
         x, y
     );
+
+    // Ignoring an Entire Value with _
+    foo(3, 4);
+
+    // Ignoring Parts of a Value with a Nested _
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {:?}", setting_value);
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {}, {}, {}", first, third, fifth)
+        }
+    }
+
+    // Ignoring an Unused Variable by Starting Its Name with _
+    let _x = 5;
+
+    let s = Some(String::from("Hello!"));
+
+    // This would give an error: if let Some(_s) = s {
+    if let Some(_) = s {
+        println!("found a string");
+    }
+
+    println!("{:?}", s);
+
+    // Ignoring Remaining Parts of a Value with ..
+    struct Point2 {
+        x: i32,
+        y: i32,
+        z: i32,
+    }
+
+    let origin = Point2 { x: 0, y: 0, z: 0 };
+
+    match origin {
+        Point2 { x, .. } => println!("x is {}", x),
+    }
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {}, {}", first, last);
+        }
+    }
+
+
+}
+
+fn foo(_: i32, y: i32) {
+    println!("This code only uses the y parameter: {}", y);
 }
