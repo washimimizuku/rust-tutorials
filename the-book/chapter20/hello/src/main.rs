87de84a0474054f0d2fc05164899c55369734e68
Nuno Barreto <nbarreto@visionapartments.com>
2022-04-19 16:18:10 +0200

    Make the web server multi threaded

--- the-book/chapter20/hello/src/main.rs
+++ the-book/chapter20/hello/src/main.rs
@@ -5,14 +5,20 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
+use hello::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         println!("Connection established!");
-        handle_connection(stream);
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
