15b665c68627cdd9cc3a737a3f11474eaccd7f66
Nuno Barreto <nbarreto@visionapartments.com>
2022-04-19 15:07:18 +0200

    Add replacement macro for vec

--- the-book/chapter19/macros/src/main.rs
+++ the-book/chapter19/macros/src/main.rs
@@ -1,3 +1,18 @@
+// Modify macro vec!
+#[macro_export]
+macro_rules! vec {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    let v: Vec<u32> = vec![1, 2, 3];
+    println!("v is {:?}", v)
 }
