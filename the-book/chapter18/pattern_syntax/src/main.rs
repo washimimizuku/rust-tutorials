e27347c91b5acd11f963c54bca15cd745bde31c4
Nuno Barreto <nbarreto@visionapartments.com>
2022-04-19 12:58:21 +0200

    Add matching literals example

--- the-book/chapter18/pattern_syntax/src/main.rs
+++ the-book/chapter18/pattern_syntax/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    // Matching Literals
+    let x = 1;
+
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
 }
