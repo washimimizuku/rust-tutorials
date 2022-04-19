8a10081bcee39a511e4755e5ed70c8a0e25713bb
Nuno Barreto <nbarreto@visionapartments.com>
2022-04-19 13:20:47 +0200

    Add destructuring enums examples

--- the-book/chapter18/pattern_syntax/src/main.rs
+++ the-book/chapter18/pattern_syntax/src/main.rs
@@ -3,6 +3,13 @@ struct Point {
     y: i32,
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32},
+    Write (String),
+    ChangeColor(i32, i32, i32),
+}
+
 fn main() {
     // Matching Literals
     let x = 1;
@@ -66,5 +73,25 @@ fn main() {
         Point {x, y: 0} => println!("On the x axis at {}", x),
         Point {x: 0, y} => println!("On the y axis at {}", y),
         Point {x, y} => println!("On neither axis: ({}, {})", x, y),
+    }
+
+    // Destructuring Enums
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.")
+        }
+        Message::Move { x, y }  => {
+            println!(
+                "Move in the x direction {} and in the y direction {}",
+                x, y
+            );
+        }
+        Message::Write(text)  => println!("Text message: {}", text),
+        Message::ChangeColor(r, g, b) => println!(
+            "Change the color to red {}, green {}, and blue {}",
+            r, g, b
+        ),
     }
 }
