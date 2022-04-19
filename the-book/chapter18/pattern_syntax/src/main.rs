376375a75285e0d7a7c556a289c2dc0d916b9393
Nuno Barreto <nbarreto@visionapartments.com>
2022-04-19 13:08:23 +0200

    Add range of values example

--- the-book/chapter18/pattern_syntax/src/main.rs
+++ the-book/chapter18/pattern_syntax/src/main.rs
@@ -29,4 +29,20 @@ fn main() {
         3 => println!("three"),
         _ => println!("anything"),
     }
+
+    // Matching Ranges of Values with ..=
+    let x = 5;
+
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
 }
