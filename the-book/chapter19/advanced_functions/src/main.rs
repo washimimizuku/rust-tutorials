326a69da7f7d593f8a0b1f1193cc208d923fde21
Nuno Barreto <nbarreto@visionapartments.com>
2022-04-19 14:57:50 +0200

    Add examples for function pointers

--- the-book/chapter19/advanced_functions/src/main.rs
+++ the-book/chapter19/advanced_functions/src/main.rs
@@ -1,3 +1,24 @@
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Function pointers
+    let answer = do_twice(add_one, 5);
+
+    println!("The answer is: {}", answer);
+
+    // Using closure
+    let list_of_numbers = vec![1, 2, 3];
+    let list_of_strings: Vec<String> =
+        list_of_numbers.iter().map(|i| i.to_string()).collect();
+
+    // Using a function pointer
+    let list_of_numbers = vec![1, 2, 3];
+    let list_of_strings: Vec<String> =
+        list_of_numbers.iter().map(ToString::to_string).collect();
 }
