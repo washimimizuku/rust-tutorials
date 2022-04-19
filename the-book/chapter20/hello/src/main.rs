7e1dc65cf2b2ff5eab36fc3318fbf055f5e642d0
Nuno Barreto <nbarreto@visionapartments.com>
2022-04-19 15:38:22 +0200

    Read the TCP request

--- the-book/chapter20/hello/src/main.rs
+++ the-book/chapter20/hello/src/main.rs
@@ -1,4 +1,6 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -7,5 +9,14 @@ fn main() {
         let stream = stream.unwrap();
 
         println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request {}", String::from_utf8_lossy(&buffer[..]));
 }
