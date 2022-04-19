fe97d8070b78b84498cebe36eca423307ca71912
Nuno Barreto <nbarreto@visionapartments.com>
2022-04-19 13:16:14 +0200

    Add destructuring structs examples

--- the-book/chapter18/pattern_syntax/src/main.rs
+++ the-book/chapter18/pattern_syntax/src/main.rs
@@ -1,3 +1,8 @@
+struct Point {
+    x: i32,
+    y: i32,
+}
+
 fn main() {
     // Matching Literals
     let x = 1;
@@ -44,5 +49,22 @@ fn main() {
         'a'..='j' => println!("early ASCII letter"),
         'k'..='z' => println!("late ASCII letter"),
         _ => println!("something else"),
+    }
+
+    // Destructuring Structs
+    let p = Point { x: 0, y: 7 };
+
+    let Point { x: a, y: b } = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+
+    let Point { x, y } = p;
+    assert_eq!(0, x);
+    assert_eq!(7, y);
+
+    match p {
+        Point {x, y: 0} => println!("On the x axis at {}", x),
+        Point {x: 0, y} => println!("On the y axis at {}", y),
+        Point {x, y} => println!("On neither axis: ({}, {})", x, y),
     }
 }
